3a54d1ed5b351924c1822f5331f45fdef89edd75
Дмитрий <dima272024@gmail.com>
2024-12-09 22:24:55 +0700

    Add files via upload

---
+++ DZ_Variables.py
@@ -0,0 +1,5 @@
+number_of_completed_DZ = 12
+number_of_hours_spent = 1.5
+course_name = 'Python'
+time_for_one_task = (12 / 1.5)
+print('Курс:', course_name,  'всего задач:',number_of_completed_DZ, 'затрачено часов:', number_of_hours_spent, 'часа', 'среднее время выполнения:', time_for_one_task,'часов')
