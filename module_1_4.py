0bafc81193d57bf5bff80eb763c6c6aa726dae0e
Дмитрий <dima272024@gmail.com>
2024-12-11 21:08:13 +0700

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,12 @@
+my_string = input('Как вас зовут? ')
+print(len(my_string))
+print('Как вас зовут?'.upper())
+print('Как вас зовут?'.lower())
+print('Как вас зовут?'.replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
+
+
+
+
+
